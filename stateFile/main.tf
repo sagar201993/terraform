df727a92b88b714010d5b27189770bf2f37987b1
sagar <124078739+sagar201993@users.noreply.github.com>
2026-06-15 12:23:06 +0200

    configure state file in s3 remotly

---
+++ stateFile/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "munish-sagar-terraform-state-20260615"
+    key          = "dev/terrafrom.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
