da07a441bdae2b93ed26c0e365c34f86101a7202
sagar <124078739+sagar201993@users.noreply.github.com>
2026-06-15 11:51:47 +0200

    first commit

---
+++ s3bucket/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# create s3 bucket 
+resource "aws_s3_bucket" "first_bucket" {
+  bucket = "my-bucket-123456-sagar"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
